85ce20959c8303c4ec6d98c7e631e27372932442
czdigitaldesigner@gmail.com <czdigitaldesigner@gmail.com>
2024-03-30 10:56:10 -0700

    Task model created

---
+++ models/Task.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const calendarEvent = new mongoose.Schema ({
+    eventName: {type: String, required: true},
+    Date: String,
+    Time: String,
+    createdBy: {type: mongoose.Types.ObjectId, requried: true}
+})
+
+const Event = mongoose.model("Event", calendarEvent)
+
+module.exports = Event 
