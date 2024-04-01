6f2cded76d4dffd145b3f21083156f43e1c8cd5e
neil-mell489 <neilmellstrom97@gmail.com>
2024-04-01 13:51:19 -0700

    rewrote server.js  a bit

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-require("dotenv").config()
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const connectDB = require('./database');
+const controllers = require('./controllers/index.js');
 
-const cors = require("cors")
-const {PORT} = process.env
-const controllers = require("./controllers/index.js")
-
-const express = require("express")
-const app = express()
+const app = express();
 
 // Middlewares
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB database
+connectDB();
 
 // Routes
+app.use('/api', controllers);
 
-app.use("/api", controllers)
+app.get('/', (req, res) => {
+    res.send('This is working');
+});
 
-app.get("/", (req,res) => {
-    res.send("This is working")
-})
-
-app.listen(PORT, () => {console.log(`Server is running on ${PORT}`)})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
