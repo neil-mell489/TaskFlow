c3165773ce9b6cfe2196221f3a7062e150dd6768
czdigitaldesigner@gmail.com <czdigitaldesigner@gmail.com>
2024-04-02 21:35:25 -0700

    JWT structured

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,16 @@
 const router = require("express").Router()
+const { verifyToken } = require("../middlewares/verifyToken")
 const eventController = require("./taskController")
+const userCtrl = require("./userController")
 
 router.get("/event", eventController.getEvent)
 router.post("/event", eventController.createEvent)
 router.put("/event/:id", eventController.updateEvent)
 router.delete("/event/:id", eventController.deleteEvent)
+
+router.post("/auth/signup", userCtrl.signup)
+router.post("/auth/login", userCtrl.login)
+router.get("/user/:id", verifyToken, userCtrl.getUser)
 
 module.exports = router
 
---
+++ controllers/userController.js
@@ -0,0 +1,73 @@
+const { createToken } = require("../middlewares/verifyToken")
+const db = require("../models")
+const bcrypt = require("bcrypt")
+
+const signup = async (req,res) => {
+    try{
+        const {email, username, password} = req.body
+        const query = db.User.find({})
+        query.or([{username: username}, {email:email}])
+        const foundUser = await query.exec()
+
+        if (foundUser.length !== 0){
+            return res.status(400).json({message: "Username and Email already taken"})
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+
+        req.body.password = hash
+        const createdUser = await db.User.create(req.body)
+        await createdUser.save()
+
+        return res.status(201).json({message: "User successfully registered", userId: createdUser.id})
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({error: "Internal Server Error", message: err.message})
+    }
+}
+
+const login = async(req,res) => {
+    try{
+        const {username,password,email} = req.body
+        const query = db.User.find({})
+        query.and([{username:username}, {email:email}])
+
+        const foundUser = await query.exec()
+        if(foundUser.length === 0){
+            return res.status(400).json({error: "Invalid Login Credentials"})
+        }
+        const verifyPassword = await bcrypt.compare(password, foundUser[0].password)
+        if(!verifyPassword){
+            return res.status(400).json({error: "Invalid Login Credentials"})
+        }
+        
+        const token = createToken(foundUser[0])
+        return res.status(200).json({token, id: foundUser[0]._id})    
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({message: "Internal Server Error", error: err.message})
+    }
+}
+
+const getUser = async(req,res) => {
+    try{
+        const id = req.params.id
+        const query = db.User.findById(id)
+        query.select("-password")
+        const foundUser = await query.exec()
+        if(!foundUser){
+            return res.status(400).json({error: "User is not found"})
+        }
+        return res.status(200).json({message: "Successfully found user", data: foundUser})
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({error: "Internal Server Error"})
+    }
+}
+
+module.exports = {
+    signup,
+    login,
+    getUser
+}
