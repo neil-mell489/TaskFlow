1eaf7507b24deb2022de0cf6ed899307fd21bac8
neil-mell489 <neilmellstrom97@gmail.com>
2024-04-02 18:55:18 -0700

    updated data type in Task.js model

--- models/Task.js
+++ models/Task.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const calendarEventSchema = new mongoose.Schema({
     eventName: { type: String, required: true },
     description: { type: String, required: true },
-    creationDate: { type: Date, default: Date.now },
-    createdAt: { type: Date, default: Date.now },
+    creationDate: { type: Number},
+    createdAt: { type: Number},
     createdBy: { type: mongoose.Schema.Types.ObjectId, required: true }
 });
+
 
 const Event = mongoose.model("Event", calendarEventSchema);
 
